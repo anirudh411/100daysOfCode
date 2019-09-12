adb407c931b5197962a06fe056d88033db0973cc
anirudh kumar <anirudh.chauhan.142@gmail.com>
2019-09-12 18:51:45 +0530

    added hands in clock

--- timerVisualisation/index.js
+++ timerVisualisation/index.js
@@ -10,13 +10,13 @@ function draw() {
     translate(800, 300);
     rotate(-90);
     noFill();
-    strokeWeight(5);
     showClocks();
-    strokeWeight(1);
-    rotate(+90);
-    fill(255);
-    let textTitle = `${hour.value.toString().length < 2 ? '0' + hour.value : hour.value} : ${minute.value.toString().length < 2 ? '0' + minute.value : minute.value} : ${seconds.value.toString().length < 2 ? '0' + seconds.value : seconds.value} `
-    text(textTitle, -25, 0);
+    updateHands();
+     let textTitle = `${hour.value.toString().length < 2 ? '0' + hour.value : hour.value} : ${minute.value.toString().length < 2 ? '0' + minute.value : minute.value} : ${seconds.value.toString().length < 2 ? '0' + seconds.value : seconds.value} `
+     fill('#fff');
+     noStroke();
+     text(textTitle, -30, -200);
+
 }
 function showClocks() {
     if (seconds.value > 0) {
@@ -29,7 +29,7 @@ function showClocks() {
         hour.show();
     }
 }
-setInterval(() => updateAngleAndValue(), 100);
+setInterval(() => updateAngleAndValue(), 1000);
 
 function updateAngleAndValue() {
     seconds.value++;
@@ -47,4 +47,29 @@ function updateAngleAndValue() {
     seconds.angle = map(seconds.value, 0, 59, 1, 360);
     minute.angle = map(minute.value, 0, 59, 1, 360);
     hour.angle = map(hour.value, 0, 59, 1, 360);
+}
+function updateHands() {
+    rotate(+90);
+    fill(255);
+    strokeWeight(4);
+    push();
+    rotate(seconds.angle);
+    stroke(seconds.color);
+    line(0, 0, 0, -110);
+    pop();
+    push();
+    stroke(minute.color);
+    rotate(minute.angle)
+    line(0, 0, 0, -90);
+    pop();
+    push()
+    stroke(hour.color);
+    rotate(hour.angle)
+    line(0, 0, 0, -70);
+    pop();
+    fill(0);
+    stroke('fff');
+    strokeWeight(8);
+    point(0, 0);
+
 }
