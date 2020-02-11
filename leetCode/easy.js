9ad81d1bcbf25633962ad385aa223ac71299d2da
anirudh <anirudh.chauhan.142@gmail.com>
2020-02-11 20:24:01 +0530

    Remove Vowels from a String

--- leetCode/easy.js
+++ leetCode/easy.js
@@ -8,3 +8,11 @@ function numberOfSteps(num = 0) {
 	else if (num % 2 == 0) return 1 + numberOfSteps(num / 2);
 	else return 1 + numberOfSteps(num - 1);
 }
+
+
+function removeVowels(str ='') {
+	if (str.length<1) return "";
+	if (["a","e","i","o","u"].includes(str[0].toLowerCase())) return  removeVowels(str.substr(1));
+	else return  str[0]+removeVowels(str.substr(1));
+}
+console.log(removeVowels("anirudh"));
