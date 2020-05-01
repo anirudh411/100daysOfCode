e94eb1163349d5e4259914ba24e45020557c8529
anirudh <anirudh.chauhan.142@gmail.com>
2020-05-01 15:13:06 +0530

    working on flock simulation

---
+++ p5Sketches/flockSimulation/sketch.js
@@ -0,0 +1,62 @@
+class Boid {
+    constructor() {
+        this.position = createVector(random(width), random(height));
+        this.velocity = p5.Vector.random2D();
+        this.velocity.setMag(random(2, 5));
+        this.acceleration = createVector();
+    }
+
+    update() {
+        this.position.add(this.velocity);
+        this.velocity.add(this.acceleration);
+        this.acceleration.mult(0);
+    }
+
+    align(boids = []) {
+        let steering = createVector();
+        const perception = 500;
+        let totalInPerception = 0;
+        for (const otherBoid of boids) {
+            const distance = dist(this.position.x, this.position.y, otherBoid.position.x, otherBoid.position.y);
+            if (otherBoid != this && distance < perception) {
+                steering.add(otherBoid.velocity);
+                totalInPerception++;
+            }
+        }
+        if (totalInPerception > 0) {
+            steering.div(totalInPerception);
+            steering.sub(this.velocity);
+        }
+
+        return steering
+    }
+
+    flock(boids) {
+        this.acceleration = this.align(boids);
+    }
+
+    show() {
+        strokeWeight(16);
+        stroke(255);
+        point(this.position.x, this.position.y);
+    }
+}
+
+const flock = []
+
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+    for (let i = 0; i < 100; i++) {
+        flock.push(new Boid());
+    }
+}
+
+
+function draw() {
+    background(0);
+    flock.forEach(boid => {
+        boid.flock(flock);
+        boid.show()
+        boid.update();
+    });
+}
